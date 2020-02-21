855afa695aa2e901e1a91dd8a9bf6b6d3a6c134c
Ivlev Sergey <best-M9lTHblu@ya.ru>
2020-02-22 02:48:28 +0300

    done

--- src/index.js
+++ src/index.js
@@ -2,47 +2,19 @@
 import readlineSync from 'readline-sync';
 
 
-// This function asks for a name from the user, otherwise the user gets an anonymous name.
-export function getName() {
-  let askName = readlineSync.question('May I have your name? ');
-  const noName = 'anonymous';
-  if (askName) {
-    console.log(`Hello, ${askName}!`);
-  } else {
-    askName = noName;
-    console.log(`Hello, ${askName}!`);
-  }
-  return askName;
-}
 
-// game
-export function game(coll, name) {
-  console.log('Answer "yes" if the number is even, otherwise answer "no".');
-  const sizeColl = coll.length;
-  for (let i = 0; i < sizeColl; i += 1) {
-    const num = coll[i];
-    const check = num % 2 === 0 ? 'yes' : 'no';
-    const answer = readlineSync.question(`Question: ${num}\nYour answer: `);
-    if (answer === check) {
-      console.log('Correct!');
-    } else {
-      console.log(`"yes" is wrong answer ;(. Correct answer was "no".\nLet's try again, ${name}`);
-      process.exit();
-    }
-  }
-  return console.log(`Congratulations, ${name}!`);
-}
+export const meetUser = () => readlineSync.question('May I have your name? ');
 
-// This function saves username
-export const saveName = (value) => (value || 'anonymous');
+export const sayHello = (name) => console.log(`Hello${name ? (', ' + name) : name}!`);
 
-// This function generates random numbers ('items' == total elements in returned array)
-export function generateRandomNum(items = 3) {
-  const returnedtArray = [];
-  while (returnedtArray.length < items) {
-    const generator = Math.floor((Math.random() * 100) / 2) + 2;
-    if (returnedtArray.includes(generator)) continue;
-    returnedtArray.push(generator);
-  }
-  return returnedtArray;
-}
+export const question = (value) => {
+  console.log(`Question: ${value}`);
+  return value;
+};
+export const termsOfGame = (str) => console.log(str);
+
+export const userAnswer = () => readlineSync.question('Your answer: ');
+
+export const checkAnswer = (value1, value2) => value1 === value2;
+
+
