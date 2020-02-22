3784f32a7c6a8b816237db0b7d020bf090413216
Ivlev Sergey <best-M9lTHblu@ya.ru>
2020-02-22 11:56:23 +0300

    валываоывоалоывлапоолывапролфывап

--- src/index.js
+++ src/index.js
@@ -4,21 +4,3 @@ import readlineSync from 'readline-sync';
 const welcome = readlineSync.question('May I have your name? ');
 
 
-<<<<<<< HEAD
-=======
-export const meetUser = () => readlineSync.question('May I have your name? ');
-
-export const sayHello = (name) => console.log(`Hello${name ? (', ' + name) : name}!`);
-
-export const question = (value) => {
-  console.log(`Question: ${value}`);
-  return value;
-};
-export const termsOfGame = (str) => console.log(str);
-
-export const userAnswer = () => readlineSync.question('Your answer: ');
-
-export const checkAnswer = (value1, value2) => value1 === value2;
-
-
->>>>>>> dev
