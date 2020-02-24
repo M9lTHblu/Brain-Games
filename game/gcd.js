c723e1710512daccde0e60a993dc20126b70b17a
Ivlev Sergey <best-M9lTHblu@ya.ru>
2020-02-24 23:26:12 +0300

    step 7/9 done!

--- game/gcd.js
+++ game/gcd.js
@@ -2,7 +2,7 @@ import { engine } from '../src/index.js';
 
 const getNum = () => Math.floor(Math.random() * (100 - 2) + 2);
 
-const getArrOfNums= () => {
+const getArrOfNums = () => {
   const arr = [];
   let count = 2;
   while (count > 0) {
@@ -14,28 +14,28 @@ const getArrOfNums= () => {
 };
 
 const getStrOfArr = () => {
-	const [first, second] = getArrOfNums();
-	const result = (second > first) ? [second,first] : [first, second];
-	return result.join(' ');
+  const [first, second] = getArrOfNums();
+  const result = (second > first) ? [second, first] : [first, second];
+  return result.join(' ');
 };
 
-const evcld = (str) => {
-	let [first, second] = str.split(' ');
-	while (true) {
-		let temp = first % second;
-		if (temp === 0) {
-			return String(second);
-		}
-		if (temp !== 0) {
-			first = second % temp;
-			if (first === 0) {
-				return String(temp);
-			} else { second = temp % first; }
-		}
-	}
+const getDivider = (str) => {
+  let [first, second] = str.split(' ');
+  while (true) {
+    const temp = first % second;
+    if (temp === 0) {
+      return String(second);
+    }
+    if (temp !== 0) {
+      first = second % temp;
+      if (first === 0) {
+        return String(temp);
+      } second = temp % first;
+    }
+  }
 };
-evcld(getStrOfArr());
+getDivider(getStrOfArr());
 
 const rule = 'Find the greatest common divisor of given numbers.';
 
-export default () => engine(rule, getStrOfArr, evcld);
+export default () => engine(rule, getStrOfArr, getDivider);
