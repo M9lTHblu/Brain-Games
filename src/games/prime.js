124c4b05bbb523052a111d6e5b2aef38403c6d30
Serge Ivlev <Liho@ro.ru>
2020-03-23 00:02:14 +0300

    chore(prime.js)

--- src/games/prime.js
+++ src/games/prime.js
@@ -1,13 +1,17 @@
 import { getRandomNum } from '../math.js';
 
-const isPrime = (question) => {
+const isPrime = (number) => {
   let result = 0;
-  for (let i = 2; i < question; i += 1) {
-    if (question % i === 0) result += i;
+  for (let i = 2; i < number; i += 1) {
+    if (number % i === 0) result += i;
   }
-  const answer = (question === result) ? 'yes' : 'no';
+  return number === result;
+};
+
+const getQuestionAndAnswer = (question) => {
+  const answer = question ? 'yes' : 'no';
   return [question, answer];
 };
 
 export const rule = 'Answer "yes" if given number is prime. Otherwise answer "no".';
-export const game = () => isPrime(getRandomNum(2, 101));
+export const game = () => getQuestionAndAnswer(isPrime(getRandomNum(2, 101)));
