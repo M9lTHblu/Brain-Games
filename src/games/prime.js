c28f43e6a5eb6bdc695348f566eb1e9c424b2e0d
Serge Ivlev <Liho@ro.ru>
2020-03-28 11:21:13 +0300

    edit(prime.js)

--- src/games/prime.js
+++ src/games/prime.js
@@ -4,9 +4,6 @@ import { getRandomNum } from '../utils.js';
 const rule = 'Answer "yes" if given number is prime. Otherwise answer "no".';
 
 const isPrime = (number) => {
-  if (number < 2) {
-    return false;
-  }
   for (let i = 2; i <= number / 2; i += 1) {
     if (number % i === 0) {
       return false;
