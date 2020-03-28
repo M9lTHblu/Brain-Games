6239d55596295e7fb8571d30a4997605ecdfe750
Serge Ivlev <Liho@ro.ru>
2020-03-28 11:12:34 +0300

    edit(prime.js)

--- src/games/prime.js
+++ src/games/prime.js
@@ -5,12 +5,10 @@ const rule = 'Answer "yes" if given number is prime. Otherwise answer "no".';
 
 const isPrime = (number) => {
   let result = 0;
-  let i = 2;
-  while (result === 0) {
+  for (let i = 2; i <= number / 2; i += 1) {
     if (number % i === 0) {
       result = i;
     }
-    i += 1;
   }
   return number === result;
 };
