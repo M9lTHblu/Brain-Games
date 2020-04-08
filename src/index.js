ca075eef62f26673eeaf34121dd8585440928e80
Serge Ivlev <Liho@ro.ru>
2020-04-08 19:28:49 +0300

    refactor(index.js, calc.js)

--- src/index.js
+++ src/index.js
@@ -4,19 +4,24 @@ import readlineSync from 'readline-sync';
 export default (description, game) => {
   console.log('Welcome to the Brain Games!');
   const name = readlineSync.question('May I have your name? ');
-  console.log(`Hello${name ? `, ${name}` : name}!`);
+  console.log(`Hello, ${name}!`);
   console.log(description);
-  let counter = 3;
-  while (counter > 0) {
+
+  let counter = 0;
+  while (counter < 3) {
     const [question, answer] = game();
     console.log(`Question: ${question}`);
     const usrAnswer = readlineSync.question('Your answer: ');
+
+    if (`${answer}` !== usrAnswer) {
+      console.log(`"${usrAnswer}" is wrong answer ;(. Correct answer was "${answer}".`);
+      console.log(`Let's try again ${name}!`);
+      return;
+    }
     if (`${answer}` === usrAnswer) {
       console.log('Correct!');
-    } else {
-      return (console.log(`"${usrAnswer}" is wrong answer ;(. Correct answer was "${answer}".\nLet's try again ${name}!`));
+      counter += 1;
     }
-    counter -= 1;
   }
-  return (console.log(`Congratulations${name ? `, ${name}` : name}!`));
+  console.log(`Congratulations, ${name}`);
 };
